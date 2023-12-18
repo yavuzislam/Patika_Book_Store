b0a33d9ead92a23c7a1080ce3122b8346ec03253
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-19 01:08:48 +0300

    Create GetBookByIdQuery.cs

---
+++ BookStore/BookStore/BookOperation/GetBooks/GetBookByIdQuery.cs
@@ -0,0 +1,42 @@
+﻿using BookStore.Common;
+using BookStore.DbOperations;
+
+namespace BookStore.BookOperation.GetBooks;
+
+public class GetBookByIdQuery
+{
+    private readonly BookStoreDbContext _dbContext;
+
+    public int BookId { get; set; }
+
+    public GetBookByIdQuery(BookStoreDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public GetBookViewModel Handle()
+    {
+        var book = _dbContext.Books.Find(BookId);
+        if (book is null)
+        {
+            throw new InvalidOperationException("Book not found!");
+        }
+
+        GetBookViewModel model = new()
+        {
+            Title = book.Title,
+            Genre = ((GenreType)book.GenreId).ToString(),
+            PageCount = book.PageCount,
+            PublishDate = book.PublishDate.Date.ToString("dd/MM/yyyy")
+        };
+        return model;
+    }
+}
+
+public class GetBookViewModel
+{
+    public string Title { get; set; }
+    public string Genre { get; set; }
+    public int PageCount { get; set; }
+    public string PublishDate { get; set; }
+}
