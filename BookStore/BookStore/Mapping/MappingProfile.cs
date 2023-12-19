6f01fd5abd7159ab01a3885781cf435b5c46e114
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-19 14:08:16 +0300

    Create MappingProfile.cs

---
+++ BookStore/BookStore/Mapping/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using BookStore.BookOperation.CreateBook;
+using BookStore.BookOperation.GetBooks;
+using BookStore.BookOperation.UpdateBook;
+using BookStore.Common;
+using BookStore.Entities;
+
+namespace BookStore.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<CreateBookModel, Book>();
+        CreateMap<Book, GetBookViewModel>().ForMember(dest => dest.Genre,
+            opt => opt.MapFrom(src => ((GenreType)src.GenreId).ToString())).ForMember(des => des.PublishDate,
+            opt => opt.MapFrom(src => src.PublishDate.Date.ToString("dd/MM/yyyy")));
+        CreateMap<Book, BooksViewModel>().ForMember(des => des.Genre,
+            opt => opt.MapFrom(src => ((GenreType)src.GenreId).ToString())).ForMember(des => des.PublishDate,
+            opt => opt.MapFrom(src => src.PublishDate.Date.ToString("dd/MM/yyyy")));
+
+        CreateMap<UpdateBookModel, Book>();
+    }
+}
