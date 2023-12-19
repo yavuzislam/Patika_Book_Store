4122794fe2aeafb0fec1bb2a6e0f71395e8607a3
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-19 20:01:37 +0300

    Create DeleteBookCommand.cs

---
+++ BookStore/BookStore/BookOperation/DeleteBook/DeleteBookCommand.cs
@@ -0,0 +1,27 @@
+﻿using BookStore.DbOperations;
+
+namespace BookStore.BookOperation.DeleteBook;
+
+public class DeleteBookCommand
+{
+    private readonly BookStoreDbContext _dbContext;
+
+    public int BookId { get; set; }
+    public DeleteBookCommand(BookStoreDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public void Handle()
+    {
+        var book = _dbContext.Books.SingleOrDefault(x => x.Id == BookId);
+        if (book is null)
+        {
+            throw new InvalidOperationException("Book not found!");
+        }
+
+        _dbContext.Books.Remove(book);
+
+        _dbContext.SaveChanges();
+    }
+}
