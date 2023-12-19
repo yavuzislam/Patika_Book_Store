0fef2de486f88e163192875800145069fe5eef2b
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-19 20:01:54 +0300

    Create GetBookByIdQueryValidator.cs

---
+++ BookStore/BookStore/BookOperation/GetBooks/GetBookByIdQueryValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace BookStore.BookOperation.GetBooks;
+
+public class GetBookByIdQueryValidator : AbstractValidator<GetBookByIdQuery>
+{
+    public GetBookByIdQueryValidator()
+    {
+        RuleFor(query => query.BookId).GreaterThan(0);
+    }
+}
