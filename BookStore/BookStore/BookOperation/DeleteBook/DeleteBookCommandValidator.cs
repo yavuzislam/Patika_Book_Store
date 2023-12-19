5b318b20026fde29cf59d2fbbcc1fd24f78f2187
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-19 20:01:42 +0300

    Create DeleteBookCommandValidator.cs

---
+++ BookStore/BookStore/BookOperation/DeleteBook/DeleteBookCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace BookStore.BookOperation.DeleteBook;
+
+public class DeleteBookCommandValidator:AbstractValidator<DeleteBookCommand>
+{
+    public DeleteBookCommandValidator()
+    {
+        RuleFor(command => command.BookId).GreaterThan(0);
+    }
+}
