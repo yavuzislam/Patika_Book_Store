a9468b86316b967d516e8fd4f5ea17f8595423d2
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-19 14:06:57 +0300

    Update GetBooksQuery.cs
    I mapped the models with the AutoMapper service.

--- BookStore/BookStore/BookOperation/GetBooks/GetBooksQuery.cs
+++ BookStore/BookStore/BookOperation/GetBooks/GetBooksQuery.cs
@@ -1,4 +1,5 @@
-﻿using BookStore.Common;
+﻿using AutoMapper;
+using BookStore.Common;
 using BookStore.DbOperations;
 using BookStore.Entities;
 
@@ -7,20 +8,23 @@ namespace BookStore.BookOperation.GetBooks;
 public class GetBooksQuery
 {
     private readonly BookStoreDbContext _dbContext;
+    private readonly IMapper _mapper;
 
-    public GetBooksQuery(BookStoreDbContext dbContext)
+    public GetBooksQuery(BookStoreDbContext dbContext, IMapper mapper)
     {
         _dbContext = dbContext;
+        _mapper = mapper;
     }
 
     public IEnumerable<BooksViewModel> Handle()
     {
         var bookList = _dbContext.Books.OrderBy(x => x.Id).ToList();
-        var vm = bookList.Select(book => new BooksViewModel()
-        {
-            Title = book.Title, Genre = ((GenreType)book.GenreId).ToString(), PageCount = book.PageCount,
-            PublishDate = book.PublishDate.Date.ToString("dd/MM/yyyy")
-        }).ToList();
+        var vm = _mapper.Map<List<BooksViewModel>>(bookList);
+        // var vm = bookList.Select(book => new BooksViewModel()
+        // {
+        //     Title = book.Title, Genre = ((GenreType)book.GenreId).ToString(), PageCount = book.PageCount,
+        //     PublishDate = book.PublishDate.Date.ToString("dd/MM/yyyy")
+        // }).ToList();
 
         return vm;
     }
