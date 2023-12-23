b05621818d8e54c742c93fde4ec8d4f09b50397a
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-23 23:54:39 +0300

    Create DbLogger.cs

---
+++ BookStore/BookStore/Services/DbLogger.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStore.Services;
+
+public class DbLogger : ILoggerService
+{
+    public void Write(string message)
+    {
+        Console.WriteLine("[DbLogger] - " + message);
+    }
+}
