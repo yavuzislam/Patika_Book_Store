d7a2449cc7a8e0a74a6786efead139076f2948e0
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-27 22:52:24 +0300

    Create Genres.cs

---
+++ BookStore/BookStoreApi.UnitTests/TestSetup/Genres.cs
@@ -0,0 +1,25 @@
+﻿using BookStore.DbOperations;
+using BookStore.Entities;
+
+namespace BookStoreApi.UnitTests.TestSetup;
+
+public static class Genres
+{
+    public static void AddGenres(this BookStoreDbContext context)
+    {
+        context.Genres.AddRange(
+            new Genre()
+            {
+                Name = "Personal Growth"
+            },
+            new Genre()
+            {
+                Name = "Science Fiction"
+            },
+            new Genre()
+            {
+                Name = "Romance"
+            }
+        );
+    }
+}
