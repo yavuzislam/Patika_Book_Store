1461563190a165ddad6bc450122fc25cf51f9efe
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-27 22:50:20 +0300

    Create DeleteAuthorCommand.cs

---
+++ BookStore/BookStore/Application/AuthorOperations/Commands/DeleteAuthor/DeleteAuthorCommand.cs
@@ -0,0 +1,28 @@
+﻿using BookStore.DbOperations;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Application.AuthorOperations.Commands.DeleteAuthor;
+
+public class DeleteAuthorCommand
+{
+    private readonly IBookStoreDbContext _dbContext;
+    public int AuthorId { get; set; }
+
+    public DeleteAuthorCommand(IBookStoreDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public void Handle()
+    {
+        var author = _dbContext.Authors.SingleOrDefault(x => x.Id == AuthorId);
+        if (author is null)
+            throw new InvalidOperationException("Author not found.");
+        var book = _dbContext.Books.SingleOrDefault(x => x.AuthorId == AuthorId);
+        if (book is not null)
+            throw new InvalidOperationException("Author has active books. Therefore, cannot be deleted.");
+
+        _dbContext.Authors.Remove(author);
+        _dbContext.SaveChanges();
+    }
+}
