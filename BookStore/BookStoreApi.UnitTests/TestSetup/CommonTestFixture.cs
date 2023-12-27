012a311072f1672d318aa8a1f3e1bc3e703c1d1e
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-27 22:52:21 +0300

    Create CommonTestFixture.cs

---
+++ BookStore/BookStoreApi.UnitTests/TestSetup/CommonTestFixture.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using BookStore.DbOperations;
+using BookStore.Mapping;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApi.UnitTests.TestSetup;
+
+public class CommonTestFixture
+{
+    public BookStoreDbContext Context { get; set; }
+    public IMapper Mapper { get; set; }
+
+    public CommonTestFixture()
+    {
+        var options = new DbContextOptionsBuilder<BookStoreDbContext>()
+            .UseInMemoryDatabase("TestDb").Options;
+
+        Context = new BookStoreDbContext(options);
+        Context.Database.EnsureCreated();
+        Context.AddBooks();
+        Context.AddAuthors();
+        Context.AddGenres(); 
+
+        Mapper = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<MappingProfile>();
+        }).CreateMapper();
+    }
+}
