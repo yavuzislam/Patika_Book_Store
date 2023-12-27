f73984f8162faf505c18633faed7e69ff14adbb6
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2023-12-27 22:50:15 +0300

    Create CreateAuthorCommandValidator.cs

---
+++ BookStore/BookStore/Application/AuthorOperations/Commands/CreateAuthor/CreateAuthorCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace BookStore.Application.AuthorOperations.Commands.CreateAuthor;
+
+public class CreateAuthorCommandValidator:AbstractValidator<CreateAuthorCommand>
+{
+    public CreateAuthorCommandValidator()
+    {
+        RuleFor(command => command.Model.Name).MinimumLength(2);
+        RuleFor(command => command.Model.Surname).MinimumLength(2);
+        RuleFor(command => command.Model.BirthDate.Date).NotEmpty().LessThan(DateTime.Now.Date);
+    }
+}
